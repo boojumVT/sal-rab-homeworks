67ba152704c2da8d68acc003fbbded4efd824bba
Vera Ten <maryana.ten@gmail.com>
2023-08-13 21:17:30 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,21 +1,8 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
-    // Задание №1.1. Оповещение о добавлении в корзину
-
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
-
-    // Конец решения задания №1.1.
-
+    let message = productName+' '+'за'+' '+productPrice+' '+'теперь в корзине!';
+    console.log(message);
     return message;
 }
 
@@ -23,7 +10,7 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
+    let newValue = oldValue+1;
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -36,6 +23,10 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum+difference;
+    let newSumText = newSum+' ₽';
+    newSumText = `${newSum} ₽`;
+
 
     // Задание №1.3. Увеличение суммы в корзине
 
